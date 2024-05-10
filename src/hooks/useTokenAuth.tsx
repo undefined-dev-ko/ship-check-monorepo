7e693c928bd42656fb8c698584b1dc7e4e5ec1e4
juri <81026531+Jang184@users.noreply.github.com>
2024-05-10 21:53:07 +0900

    fix: token key 값 변경 (#18)

--- src/hooks/useTokenAuth.tsx
+++ src/hooks/useTokenAuth.tsx
@@ -9,30 +9,23 @@ const extractUserFromAccessToken = (accessToken: string | undefined) => {
   return JSON.parse(decodedPayload) as User;
 };
 
-const STORAGE_KEY_JWT = 'bearerToken';
-
-const getToken = (): TokenPair | undefined => {
-  return JSON.parse(localStorage.getItem(STORAGE_KEY_JWT));
+const getToken = (): string | undefined => {
+  return localStorage.getItem('accessToken');
 };
 
 /** #FIXME 로그아웃시 isLoggedIn이 제대로 false 처리가 안됨. Provider나 recoil로 변경해야할듯 */
 export const useTokenAuth = () => {
   const [tokenPair, setTokenPair] = useState<TokenPair>();
 
-  useEffect(() => {
-    const token = getToken();
-    if (!tokenPair && token) {
-      setTokenPair(structuredClone(token));
-    }
-  }, [tokenPair]);
-
   const storeToken = (token: TokenPair) => {
     setTokenPair(structuredClone(token));
-    localStorage.setItem(STORAGE_KEY_JWT, JSON.stringify(token));
+    localStorage.setItem('accessToken', token.accessToken);
+    localStorage.setItem('refreshToken', token.refreshToken);
   };
 
   const clearToken = () => {
-    localStorage.removeItem(STORAGE_KEY_JWT);
+    localStorage.removeItem('accessToken');
+    localStorage.removeItem('refreshToken');
     setTokenPair(undefined);
     window.location.reload(); //  #FIXME 로그아웃시 isLoggedIn이 제대로 false 처리가 안됨. Provider나 recoil로 변경해야할듯 > 이 이슈 때문에 임시로 새로고침을 한다
   };
