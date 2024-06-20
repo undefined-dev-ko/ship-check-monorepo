1e7fb9b96b8febe63fe9089bb938bdb5f8b22da3
양찬규 <mrriey00@naver.com>
2024-06-20 23:49:42 +0900

    chore: 환경변수 PORT 사용하도록 수정

--- src/main.ts
+++ src/main.ts
@@ -56,6 +56,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api/docs", app, document);
 
-  await app.listen(8080);
+  await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
