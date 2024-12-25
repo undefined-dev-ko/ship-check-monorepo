72a617158f7c7fd992409e9db940b05eb6750efd
jang184 <e2joo418@gmail.com>
2024-12-25 15:21:28 +0900

    env에 docker image tag 추가

--- apps/auth-api/scripts/start_container.sh
+++ apps/auth-api/scripts/start_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 환경변수 설정
-source /home/ec2-user/.env
+source /home/ec2-user/app/.env
 
 # AWS ECR 로그인
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com
 
 # ECR에서 이미지 다운로드 및 실행
-docker pull ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}:latest
-docker run -d -p 8080:8080 ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}:latest
+docker pull ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}:${IMAGE_TAG}
+docker run -d -p 8080:8080 ${ECR_REGISTRY}.dkr.ecr.ap-northeast-2.amazonaws.com/${ECR_REPOSITORY}:${IMAGE_TAG}
